4092b30564c49c514e70c31b63ff49d379e7c1fb
SouthBegonia <1687634916@qq.com>
2018-04-27 10:33:43 +0800

    添加Bellman-Ford算法说明

--- 最短路径/Bellman-Ford算法/Bellman-Ford算法.cpp
+++ 最短路径/Bellman-Ford算法/Bellman-Ford算法.cpp
@@ -1,6 +1,6 @@
 /*
 	程序功能：解决带有负权边的图（边的权值为负）。
-	Ballman-Ford算法：对所有的边进行n-1次“松弛”操作。
+	 Ballman-Ford算法：对所有的边进行n-1次“松弛”操作。
 	概念图： C code\Code_2018\最短路径\Bellman-Ford算法\Ballman-Ford算法.jpg
 */
 #include<stdio.h>
@@ -10,6 +10,7 @@ int main()
 	int bak[10], i, k;
 	int n = 5, m = 5;
 	int u[10] = { 0,2,1,1,4,3 }, v[10] = { 0,3,2,5,5,4 }, w[10] = { 0,2,-3,5,2,3 };
+	/*	u[]、v[]、w[]组成的给出边的信息的顺序可随意，因为不影响松弛的结果，但各边信息必须对应，如 2 3 2、1 2 -3 等	*/
 	int check, flag;
 	int inf = 999999;
 
@@ -27,6 +28,15 @@ int main()
 		for (i = 1; i <= m; i++)							// 进行 1 轮松弛
 			if (dis[v[i]] > dis[u[i]] + w[i])
 				dis[v[i]] = dis[u[i]] + w[i];
+		/*
+			根据键入的各边信息，则30行的比较为：
+				dis[3]	dis[2]+2
+				dis[2]	dis[1]-3
+				dis[5]	dis[1]+5
+				dis[5]	dis[4]+2
+				dis[4]	dis[3]+3
+			需要说明的是：每次松弛，dis[]值会改变，例如：在进行第2轮（k=2）松弛时，33行能松弛dis[3]，当进行到37行时，dis[3]也相应改变。
+		*/
 		check = 0;
 		for(i=1;i<=n;i++)									// 如果dis数组没有更新（松弛），则提前退出
 			if (bak[i] != dis[i])
