258ede2e182a08a3d7054157d16ddeaa178d5ace
SouthBegonia <1687634916@qq.com>
2018-10-07 16:00:10 +0800

    模块与包

---
+++ Lua/module.lua
@@ -0,0 +1,26 @@
+--[[Lua的模块由 变量，函数等已知元素组成table
+所以创建模块的方法就是创建table，把需要导出的常量，函数放入其中，最后放回table--]]
+
+
+-- 文件名 module.lua
+--定义一个名为 module 的模块
+module = {}
+
+-- 定义一个常量
+module.constant = "这是一个常量"
+
+-- 定义一个函数
+function module.func1( )
+	io.write("这是一个共有函数\n")
+end
+
+local function func2(  )
+	print("这是一个私有函数\n")
+end
+
+function module.func3(  )
+	func2()
+end
+
+return module
+
---
+++ Lua/test_module.lua
@@ -0,0 +1,5 @@
+-- Lua提供 require 函数来加载模块 require("<模块名>")
+--test_module.lua 文件
+require("module")
+print(module.constant)
+module.func3()
