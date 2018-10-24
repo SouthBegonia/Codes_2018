06abc653a88f825a736edfe0593b515771c7550d
SouthBegonia <1687634916@qq.com>
2018-10-24 18:21:21 +0800

    数组指针_完善说明

--- Pointer/PointerWithArray/README.md
+++ Pointer/PointerWithArray/README.md
@@ -17,8 +17,8 @@
 
 ## 数组和指针的差别
 1.`array[i]` 与 `*(array+i)` 结果相同，生成的机器码却不同
-- `array[i]`：从 *array* 位置开始，移动 *i* 个位置，再取出内容
-- `*(array+i)`：从 *array* 位置开始，在地址上增加 *i* ，然后取出这个地址中的内容
+- `array[i]`：从 `array` 位置开始，移动 `i` 个位置，再取出内容
+- `*(array+i)`：从 `array` 位置开始，在地址上增加 `i` ，然后取出这个地址中的内容
 
 2.`sizeof`操作符对他们的操作不同
 有代码例如
@@ -31,16 +31,30 @@ int *p = array;
 - `sizeof(p)`：返回4，即**指针长度**
 
 ## 数组指针(行指针)
-- 定义：**int (*p)[n]**
+- 定义：指的是**数组名的指针** 、 **数组首元素地址的指针** 、 **指向数组的指针**。例：`int (*p)[4]` ，`p` 即为指向数组的指针
 - 声明解释：定义一个 指向整数型的一维数组(长度为n) 的**指针**
-- 应用：
+
+有代码如下：
 ```
 int array[3][4];
 int (*p)[4];
 p = array;
 ```
-- `++p`: `p` 由行 `array[0][]`指向行`array[1][]`，也可直接理解为 `array[0][0]`指向 `array[1][0]`
-- `++array`：数组不等于指针，不存在指向，错误信息`[Error] lvalue required as increment operand`
-- `p+i`：等价于 `array+i` 和 `p[i]`，同理也是 `array[0][]`到`array[2][]`的过程 
 
-代码事例：
+** p 与 p+1 与 ++p**：`p` 是一个int型数组(长度4)的指针，`p+1` 是另一个int型数组(长度为4)的指针，通过一个 `p` 可以读取 `array[]` 的内容，`++p`同理
+![](https://i.imgur.com/nEObzYh.jpg)
+
+** p 读取 array[0]**：当数据类型匹配时，可以理解为 `*p` 内的 `*p+0,*p+1,*p+2,*p+3` 对应读取`array[0][1],array[2]...`
+![](https://i.imgur.com/8yxHeen.jpg)
+
+** p+1读取 array[1]**：当数类型匹配时，同理读取 `array[1]`的内容
+![](https://i.imgur.com/V6pYYdQ.jpg)
+
+**当 *p 与 array[]数据类型不匹配**：即`array[3][4]` 与 `(*p)[5]`，因为`*p`为5个int的数组，在`p`读取`array[0]`时，`*p+4` 占据了 `array[1][0]`，在`p+1`读取`array[1]`时，则从`array[1][1]`开始读取，产生其他结果，但这并不算错误，而是警告`[Warning] assignment from incompatible pointer type [-Wincompatible-pointer-types]`。同理，当`*p`内元素少于数组，则提前读取
+![](https://i.imgur.com/WzERE64.jpg)
+
+**总结(前提为数据类型匹配)**：
+- `p+i 与 *p+i`：一个指向第二数组首位元素，一个指向第一数组的第`i`个元素
+- `*p[n] 与 (*p)[n]`：优先级`() > [] > *`，一个是`p[n][0]/array[n][0]`，一个为`p[0][n]/array[0][n]`
+
+代码示例：
--- Pointer/PointerWithArray/数组指针.c
+++ Pointer/PointerWithArray/数组指针.c
@@ -1,3 +1,5 @@
+/*数组指针: int (*p)[n]
+当数组指针指向另一个数组时，要求数据类型匹配，否则出现警告甚至错误*/
 #include<stdio.h>
 
 int main()
@@ -7,36 +9,58 @@ int main()
 		{21,22,23,24},
 		{31,32,33,34}
 	};
-	int (*p)[4];
+	int (*p)[4];    //不匹配的数据类型，正确格式：(*p)[4]
 	p = array;
+	
 	
 	/*打印数组 array 各值及地址*/
 	for(int i=0;i<3;i++)
 	{
 		for(int j=0;j<4;j++)
-			printf("array[%d][%d]=%d  Address=%p |",i,j,array[i][j],&array[i][j]);
+			printf("array[%d][%d]=%d  ADS=%p |",i,j,array[i][j],&array[i][j]);
 		printf("\n");
 	}
 	printf("\n");
 	
-	/* ++p */
-	printf("++p = %p\n",++p);
-	printf("p   = %p\n\n",p);   //注意，一旦进行指针自加自减运算即改变了其指向
 	
-	/* p+i */
-	p = array;
-	for(int i=0;i<3;i++)
-		printf("p+%d = %p\n",i,p+i);    
-	for(int i=0;i<3;i++)        //等价于 array+i
-		printf("array[%d] = %p\n",i,array+i);
-	printf("\n");
+	/* ++p 与 p+i
+	p指向一个匿名数组 (*p)
+	p+1 或者 ++p 的差值 = 数组类型大小(sizeof(int [5]) = 4x5 =20) x i */
+	printf("###### ++p 与 p+i  ######\n");
+	printf("p   = %p\n",p);
+	printf("++p = %p\n",++p);   //差值=20x1
+	printf("p   = %p\n",p);   //注意，一旦进行指针自加自减运算即改变了其指向
 	
-	/* p[i] */
 	p = array;
 	for(int i=0;i<4;i++)
-		printf("p[%d] = %p\n",i,p[i]);
-	printf("\n");
+		printf("p+%d  = %p     *(p+%d) = %p  p[%d]= %p \n",i,p+i,i,*(p+i),i,p[i]);
+	printf("\n\n");
 	
+	
+	/* p指向的数组
+	读取 array[0] 的内容*/
+	printf("###### p 指向的数组 ######\n");
+	for(int i=0;i<4;i++)
+		printf("*p+%d =%p    *(*p+%d) =%d   (*p)[%d] =%d   ==   &array[0][%d] =%p  array[0][%d] =%d\n",i,(*p+i),i,*(*p+i),i,(*p)[i],i,&array[0][i],i,array[0][i]);
+	printf("\n\n");
+
+	
+    /* p+1 指向的另一个数组
+    读取 array[1] 的内容
+	但发现不是预想的 p+1 对称指向array[1][0]，这就是因为 p 指向为 int [5]类型，而数组为 array[][4] 类型，p内末位 *p+4 占据了 array[1][0]
+	同理，p+2 时的首位元素也被 p+1 内的末位元素占据*/
+	printf("###### p+1 指向的数组 ######\n");
+	for(int i=0;i<4;i++)
+        printf("*(p+1)+%d =%p  *(*(p+1)+%d) =%d\n",i,*(p+1)+i,i,*(*(p+1)+i));
+	printf("被占据的array[1][0] : *p+4 = %p   *(*p+4) = %d",*p+4,*(*p+4));
+	printf("\n\n");
+	
+	
+	/* *p[] 与 (*p)[] 尽量修改数据类型匹配防止发生错误 */
+	printf("###### *p[] 与 (*p)[] ######\n");
+	for(int i=0;i<3;i++)
+		printf("*p[%d] = %d   (*p)[%d] = %d\n",i,*(p[i]),i,(*p)[i]);
+	printf("\n");
 	
 	getchar();
 	return 0;
